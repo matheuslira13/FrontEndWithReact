7cce29fd0ac1bfc25f450ef8c9c4f70e76161dda
matheuslira13 <matheus_lira13@hotmail.com>
2021-01-05 13:03:11 -0300

    primeiro commit

---
+++ firtsproject/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.css';
+
+import ReactDom from 'react-dom';
+import App from './components/app';
+
+ReactDom.render(<App/>, document.getElementById('rood'));
+registerServiceWorker()
